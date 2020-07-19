913cce553dc85b32cd3d2e4b881254f6b0f50777
Makena <soniamakena02@gmail.com>
2020-07-19 21:02:41 +0300

    created control flows

--- AKAN/script.js
+++ AKAN/script.js
@@ -2,7 +2,7 @@ function getAkanName() {
   var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; 
   var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; 
   var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
-  
+
   var day = parseInt(document.getElementById("date").value);
   var month = parseInt(document.getElementById("month").value);
   var year = parseInt(document.getElementById("year").value);
@@ -10,6 +10,12 @@ function getAkanName() {
   var maleGender = document.getElementById("maleGender");
   var date = new Date(year + "-" + month + "-" + day);
   var dayBorn = date.getDay();
+  
+  if (day < 1 || day > 31) {
+    alert("Day must be greater than 1 and less than 31!"); // checks inputted day
+  }
+
+
 
 
    
