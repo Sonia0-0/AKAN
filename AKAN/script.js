564380ea5e591f5de58b8cb0257bce602fe26017
Makena <soniamakena02@gmail.com>
2020-07-19 20:58:12 +0300

    created variables

--- AKAN/script.js
+++ AKAN/script.js
@@ -1,7 +1,17 @@
 function getAkanName() {
-  var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; // array of male names
-  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; // array of female names
-  var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; // array of days
+  var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; 
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; 
+  var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; 
+  
+  var day = parseInt(document.getElementById("date").value);
+  var month = parseInt(document.getElementById("month").value);
+  var year = parseInt(document.getElementById("year").value);
+  var femaleGender = document.getElementById("femaleGender");
+  var maleGender = document.getElementById("maleGender");
+  var date = new Date(year + "-" + month + "-" + day);
+  var dayBorn = date.getDay();
+
+
    
 
    
