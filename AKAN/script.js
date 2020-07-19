a6cf2ee528f2762de65e3abb6d68499539d7f022
Makena <soniamakena02@gmail.com>
2020-07-19 20:50:50 +0300

    created arrays

---
+++ AKAN/script.js
@@ -0,0 +1,9 @@
+function getAkanName() {
+  var maleNames = ["Kwasi", "Kwado", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"]; // array of male names
+  var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]; // array of female names
+  var weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]; // array of days
+   
+
+   
+  }
+  
